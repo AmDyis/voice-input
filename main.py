684be141f21a267df8f8ea580460f35336df2962
AmDyis <86226916+AmDyis@users.noreply.github.com>
2022-07-24 13:20:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import speech_recognition as s_r
 import pyaudio
-import wave
+
 
 def record():
     r = s_r.Recognizer()
-    while True:
-        with s_r.Microphone(device_index = 1) as source:
-            print("say something")
-            r.pause_threshold = 1
-            r.adjust_for_ambient_noise(source, duration=0.7)
-            audio = r.listen(source)
-        try:
-            query = r.recognize_google(audio, language = "ru-RU")
-            print(f"you say: {query}")
-        except:
-            pass
-record()
+    with s_r.Microphone(device_index = 1) as source:
+        print("say something")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=0.7)
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio, language = "ru-RU")
+        print(f"you say: {query}")
+    except:
+        pass
+
+while True:
+    record()
 
 
